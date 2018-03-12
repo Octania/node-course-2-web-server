ceab5d1d58f78ca92e3854c8c0981696158bd695
DRC-PJudson <35580463+DRC-PJudson@users.noreply.github.com>
2018-03-12 12:02:04 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const fs = require('fs');
+
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((request, respond, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`;
+  //console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+hbs.registerHelper('getCurrentYear', function() {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function(text) {
+  return text.toUpperCase();
+});
+// app.get('/', function(request, respond){
+//   respond.send({
+//
+//     name: 'Patrick Judson',
+//     like: [
+//       'Biking',
+//       'Cities'
+//     ]
+//   });
+// });
+
+app.use(function(request, respond, next) {
+  respond.render('maintenance.hbs');
+});
+
+app.get('/', function(request, respond) {
+  respond.render('home.hbs', {
+  pageTitle: 'Home Page',
+  welcomeMessage: 'Welcome to the world!'
+});
+});
+
+app.get('/about', function(request, respond) {
+  respond.render('about.hbs', {
+  pageTitle: 'About Page'
+});
+});
+
+  app.get('/bad', function(request, respond) {
+    respond.send({
+      errorMessage: 'Unable to handle request'
+    });
+  });
+
+app.listen(3000, function(){
+  console.log('Server is running on port 3000: ');
+});
